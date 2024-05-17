b5cf4bc121bf38eb6ac9b6550d47dcbe2e927441
dnfebri <dwinofebri@gmail.com>
2024-05-17 12:49:31 +0700

    fix: navigation

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -1,3 +1,9 @@
+@php
+$navigation = [
+['href' => 'dashboard', 'label' => 'Dashboard'],
+['href' => 'inventory', 'label' => 'Inventory'],
+];
+@endphp
 <nav x-data="{ open: false }" class="bg-zinc-800 dark:bg-gray-800 border-b border-gray-100 dark:border-gray-700">
   <!-- Primary Navigation Menu -->
   <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
@@ -12,15 +18,12 @@
 
         <!-- Navigation Links -->
         <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
-          <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
-            {{ __('Dashboard') }}
+          @foreach ($navigation as $row)
+
+          <x-nav-link :href="route($row['href'])" :active="request()->routeIs($row['href'])">
+            {{ __($row['label']) }}
           </x-nav-link>
-          <x-nav-link :href="route('home')" :active="request()->routeIs('home')">
-            {{ __('Home') }}
-          </x-nav-link>
-          <x-nav-link :href="route('inventory')" :active="request()->routeIs('inventory')">
-            {{ __('Inventory') }}
-          </x-nav-link>
+          @endforeach
         </div>
       </div>
 
@@ -72,15 +75,12 @@
   <!-- Responsive Navigation Menu -->
   <div :class="{'block': open, 'hidden': ! open}" class="hidden sm:hidden">
     <div class="pt-2 pb-3 space-y-1">
-      <x-responsive-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
-        {{ __('Dashboard') }}
+
+      @foreach ($navigation as $row)
+      <x-responsive-nav-link :href="route($row['href'])" :active="request()->routeIs($row['href'])">
+        {{ __($row['label']) }}
       </x-responsive-nav-link>
-      <x-responsive-nav-link :href="route('home')" :active="request()->routeIs('home')">
-        {{ __('Home') }}
-      </x-responsive-nav-link>
-      <x-responsive-nav-link :href="route('inventory')" :active="request()->routeIs('inventory')">
-        {{ __('Inventory') }}
-      </x-responsive-nav-link>
+      @endforeach
     </div>
 
     <!-- Responsive Settings Options -->
